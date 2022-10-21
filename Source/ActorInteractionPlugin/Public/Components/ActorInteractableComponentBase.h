8ac3aab7823dafd1a213642c3bfd90ac12a4dc38
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-10-21 21:50:03 +0200

    Updated ActorInteractableComponentBase.h
    - added UFUNCTIONS

--- Source/ActorInteractionPlugin/Public/Components/ActorInteractableComponentBase.h
+++ Source/ActorInteractionPlugin/Public/Components/ActorInteractableComponentBase.h
@@ -24,59 +24,99 @@ protected:
 
 protected:
 
+	UFUNCTION()
 	virtual bool DoesAutoActive() const override;
+	UFUNCTION()
 	virtual void ToggleAutoActivate(const bool NewValue) override;
 
+	UFUNCTION()
 	virtual bool DoesAutoSetup() const override;
+	UFUNCTION()
 	virtual void ToggleAutoSetup(const bool NewValue) override;
 
+	UFUNCTION()
 	virtual bool ActivateInteractable(FString& ErrorMessage) override;
+	UFUNCTION()
 	virtual bool WakeUpInteractable(FString& ErrorMessage) override;
+	UFUNCTION()
 	virtual bool SuppressInteractable(FString& ErrorMessage) override;
+	UFUNCTION() 
 	virtual void DeactivateInteractable() override;
 
+	UFUNCTION()
 	virtual EInteractableState GetState() const override;
+	UFUNCTION()
 	virtual void SetState(const EInteractableState& NewState) override;
-	
+
+	UFUNCTION()
 	virtual TScriptInterface<IActorInteractorInterface> GetInteractor() const override;
+	UFUNCTION()
 	virtual void SetInteractor(const TScriptInterface<IActorInteractorInterface> NewInteractor) override;
 
+	UFUNCTION()
 	virtual int32 GetInteractableWeight() const override;
+	UFUNCTION()
 	virtual void SetInteractableWeight(const int32 NewWeight) override;
 
+	UFUNCTION()
 	virtual UObject* GetInteractableOwner() const override;
+	UFUNCTION()
 	virtual void SetInteractableOwner(const UObject* NewOwner) override;
 
+	UFUNCTION()
 	virtual ECollisionChannel GetCollisionChannel() const override;
+	UFUNCTION()
 	virtual void SetCollisionChannel(const ECollisionChannel& NewChannel) override;
 
+	UFUNCTION()
 	virtual TArray<UPrimitiveComponent*> GetCollisionComponents() const override;
+	UFUNCTION()
 	virtual void AddCollisionComponent(const UPrimitiveComponent* CollisionComp) override;
+	UFUNCTION()
 	virtual void AddCollisionComponents(const TArray<UPrimitiveComponent*> NewCollisionComponents) override;
+	UFUNCTION()
 	virtual void RemoveCollisionComponent(const UPrimitiveComponent* CollisionComp) override;
+	UFUNCTION()
 	virtual void RemoveCollisionComponents(const TArray<UPrimitiveComponent*> RemoveCollisionComponents) override;
 
+	UFUNCTION()
 	virtual TArray<UMeshComponent*> GetHighlightableComponents() const override;
+	UFUNCTION()
 	virtual void AddHighlightableComponent(const UMeshComponent* HighlightableComp) override;
+	UFUNCTION()
 	virtual void AddHighlightableComponents(const TArray<UMeshComponent*> HighlightableComponents) override;
+	UFUNCTION()
 	virtual void RemoveHighlightableComponent(const UMeshComponent* HighlightableComp) override;
+	UFUNCTION()
 	virtual void RemoveHighlightableComponents(const TArray<UMeshComponent*> HighlightableComponents) override;
 	
 
-	
+
+	UFUNCTION()
 	virtual UMeshComponent* FindMeshByTag(const FName Tag) const override;
+	UFUNCTION()
 	virtual UPrimitiveComponent* FindPrimitiveByTag(const FName Tag) const override;
 
+	UFUNCTION()
 	virtual TArray<FName> GetCollisionOverrides() const override;
+	UFUNCTION()
 	virtual void AddCollisionOverride(const FName Tag) override;
+	UFUNCTION()
 	virtual void AddCollisionOverrides(const TArray<FName> Tags) override;
+	UFUNCTION()
 	virtual void RemoveCollisionOverride(const FName Tag) override;
+	UFUNCTION()
 	virtual void RemoveCollisionOverrides(const TArray<FName> Tags) override;
 
+	UFUNCTION()
 	virtual TArray<FName> GetHighlightableOverrides() const override;
+	UFUNCTION()
 	virtual void AddHighlightableOverride(const FName Tag) override;
+	UFUNCTION()
 	virtual void AddHighlightableOverrides(const TArray<FName> Tags) override;
+	UFUNCTION()
 	virtual void RemoveHighlightableOverride(const FName Tag) override;
+	UFUNCTION()
 	virtual void RemoveHighlightableOverrides(const TArray<FName> Tags) override;
 
 	/**
