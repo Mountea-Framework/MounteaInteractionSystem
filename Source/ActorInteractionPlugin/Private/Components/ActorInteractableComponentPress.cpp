79196534fd1b0ec16cf5a91e503dd868085950db
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-10-30 18:40:18 +0100

    Create ActorInteractableComponentPress
    - Press key Interaction subclass

---
+++ Source/ActorInteractionPlugin/Private/Components/ActorInteractableComponentPress.cpp
@@ -0,0 +1,29 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+
+#include "Components/ActorInteractableComponentPress.h"
+
+
+// Sets default values for this component's properties
+UActorInteractableComponentPress::UActorInteractableComponentPress()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	InteractionPeriod = -1.f;
+}
+
+void UActorInteractableComponentPress::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetInteractionPeriod(-1.f);
+	
+	Timer_Interaction.Invalidate();
+}
+
+void UActorInteractableComponentPress::InteractionStarted(const float& TimeStarted)
+{
+	Super::InteractionStarted(TimeStarted);
+
+	OnInteractionCompleted.Broadcast(TimeStarted);
+}
