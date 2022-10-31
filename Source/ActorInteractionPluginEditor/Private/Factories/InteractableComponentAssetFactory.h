29eda19f9280e232b312c49bdf9508d2d102f004
Dominik Pavlíček <aenszguhelem@gmail.com>
2022-10-31 11:35:34 +0100

    Create InteractableComponentAssetFactory

---
+++ Source/ActorInteractionPluginEditor/Private/Factories/InteractableComponentAssetFactory.h
@@ -0,0 +1,22 @@
+// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "InteractableComponentAssetFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTORINTERACTIONPLUGINEDITOR_API UInteractableComponentAssetFactory : public UFactory
+{
+	GENERATED_BODY()
+	
+public:
+
+	UInteractableComponentAssetFactory();
+	
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+};
