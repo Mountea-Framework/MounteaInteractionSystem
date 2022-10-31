3e407b97f72f4ca1df257aa6a11321cdf0f028ce
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-10-31 19:09:58 +0100

    Update InteractableComponentAssetFactory.h
    - updated header

--- Source/ActorInteractionPluginEditor/Private/Factories/InteractableComponentAssetFactory.h
+++ Source/ActorInteractionPluginEditor/Private/Factories/InteractableComponentAssetFactory.h
@@ -16,7 +16,14 @@ class ACTORINTERACTIONPLUGINEDITOR_API UInteractableComponentAssetFactory : publ
 	
 public:
 
-	UInteractableComponentAssetFactory();
+	UInteractableComponentAssetFactory(const FObjectInitializer& ObjectInitializer);
+
+	virtual bool ConfigureProperties() override;
 	
 	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+
+private:
+	// Holds the template of the class we are building
+	UPROPERTY()
+	TSubclassOf<class UActorInteractableComponentBase> ParentClass;
 };
