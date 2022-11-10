095678437ff04014e407d4584c85b9c9bbc3947e
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-11-10 17:55:38 +0100

    Cleanup

--- Source/ActorInteractionPlugin/Private/Interfaces/ActorInteractorInterface.cpp
+++
@@ -1,6 +0,0 @@
-// All rights reserved Dominik Pavlicek 2022.
-
-
-#include "Interfaces/ActorInteractorInterface.h"
-
-// Add default functionality here for any IActorInteractorInterface functions that are not pure virtual.
