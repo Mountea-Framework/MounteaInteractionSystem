08388d8fcd8f37fbb49005eacd05ba113f265d19
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-11-12 22:28:54 +0100

    Create ActorInteractorComponentOverlap.cpp

---
+++ Source/ActorInteractionPlugin/Private/Components/ActorInteractorComponentOverlap.cpp
@@ -0,0 +1,9 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+
+#include "Components/ActorInteractorComponentOverlap.h"
+
+
+UActorInteractorComponentOverlap::UActorInteractorComponentOverlap()
+{
+}
