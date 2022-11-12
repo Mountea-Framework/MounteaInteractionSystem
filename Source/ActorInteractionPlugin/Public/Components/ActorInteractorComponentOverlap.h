a1ba159c274475bee7729b4fefd8a0c0ae130d06
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-11-12 22:28:13 +0100

    Create ActorInteractorComponentOverlap.h

---
+++ Source/ActorInteractionPlugin/Public/Components/ActorInteractorComponentOverlap.h
@@ -0,0 +1,18 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActorInteractorComponentBase.h"
+#include "ActorInteractorComponentOverlap.generated.h"
+
+
+UCLASS(ClassGroup=(Interaction), Blueprintable, hideCategories=(Collision, AssetUserData, Cooking, ComponentTick, Activation, Rendering), meta=(BlueprintSpawnableComponent, DisplayName = "Interactor Component Trace"))
+class ACTORINTERACTIONPLUGIN_API UActorInteractorComponentOverlap : public UActorInteractorComponentBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UActorInteractorComponentOverlap();
+};
