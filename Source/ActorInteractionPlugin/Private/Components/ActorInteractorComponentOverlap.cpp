dec313ad56e04c57d20dc952a384f75df32168c9
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-11-13 16:04:19 +0100

    Delete ActorInteractorComponentOverlap.cpp

--- Source/ActorInteractionPlugin/Private/Components/ActorInteractorComponentOverlap.cpp
+++
@@ -1,9 +0,0 @@
-﻿// All rights reserved Dominik Pavlicek 2022.
-
-
-#include "Components/ActorInteractorComponentOverlap.h"
-
-
-UActorInteractorComponentOverlap::UActorInteractorComponentOverlap()
-{
-}
