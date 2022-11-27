d649c08905459ff6447bc5ee28aebfc725ffd4ff
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-11-27 17:06:38 +0100

    Create ActorInteractableComponentMash

---
+++ Source/ActorInteractionPlugin/Private/Components/ActorInteractableComponentMash.cpp
@@ -0,0 +1,18 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+
+#include "Components/ActorInteractableComponentMash.h"
+
+#define LOCTEXT_NAMESPACE "ActorInteractableComponentMash"
+
+UActorInteractableComponentMash::UActorInteractableComponentMash()
+{
+	InteractableName = LOCTEXT("ActorInteractableComponentMash", "Mash");
+}
+
+void UActorInteractableComponentMash::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/ActorInteractionPlugin/Public/Components/ActorInteractableComponentMash.h
@@ -0,0 +1,23 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActorInteractableComponentBase.h"
+#include "ActorInteractableComponentMash.generated.h"
+
+
+UCLASS(ClassGroup=(Interaction), Blueprintable, hideCategories=(Collision, AssetUserData, Cooking, Activation), meta=(BlueprintSpawnableComponent, DisplayName = "Interactable Component Mash"))
+class ACTORINTERACTIONPLUGIN_API UActorInteractableComponentMash : public UActorInteractableComponentBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UActorInteractableComponentMash();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+};
