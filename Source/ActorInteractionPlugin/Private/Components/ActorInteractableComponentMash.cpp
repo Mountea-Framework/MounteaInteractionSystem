5902f9baccb385b4f8a5f496c1194e815568c9df
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-12-01 19:04:31 +0100

    Update ActorInteractableComponentMash

--- Source/ActorInteractionPlugin/Private/Components/ActorInteractableComponentMash.cpp
+++ Source/ActorInteractionPlugin/Private/Components/ActorInteractableComponentMash.cpp
@@ -7,12 +7,114 @@
 
 UActorInteractableComponentMash::UActorInteractableComponentMash()
 {
+	MinMashAmountRequired = 5;
+	KeystrokeTimeThreshold = 0.1f;
+	ActualMashAmount = 0;
 	InteractableName = LOCTEXT("ActorInteractableComponentMash", "Mash");
 }
 
 void UActorInteractableComponentMash::BeginPlay()
 {
 	Super::BeginPlay();
+
+	OnInteractionFailed.AddUniqueDynamic(this, &UActorInteractableComponentMash::InteractionFailed);
+}
+
+void UActorInteractableComponentMash::InteractionFailed()
+{
+	SetState(EInteractableStateV2::EIS_Awake);
+	
+	CleanUpComponent();
+	
+	OnInteractionFailedEvent();
+}
+
+void UActorInteractableComponentMash::OnInteractionFailedCallback()
+{
+	OnInteractionFailed.Broadcast();
+}
+
+void UActorInteractableComponentMash::CleanUpComponent()
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TimerHandle_Mashed);
+	}
+
+	ActualMashAmount = 0;
+}
+
+void UActorInteractableComponentMash::InteractionStarted(const float& TimeStarted, const FKey& PressedKey)
+{
+	Super::InteractionStarted(TimeStarted, PressedKey);
+
+	if (GetWorld())
+	{
+		if (GetWorld()->GetTimerManager().IsTimerActive(TimerHandle_Mashed))
+		{
+			GetWorld()->GetTimerManager().ClearTimer(TimerHandle_Mashed);
+		}
+		
+		FTimerDelegate Delegate;
+		Delegate.BindUObject(this, &UActorInteractableComponentMash::OnInteractionFailedCallback);
+		GetWorld()->GetTimerManager().SetTimer
+		(
+			TimerHandle_Mashed,
+			Delegate,
+			KeystrokeTimeThreshold,
+			false
+		);
+
+		ActualMashAmount++;
+	}
+}
+
+void UActorInteractableComponentMash::InteractionStopped(const float& TimeStarted, const FKey& PressedKey)
+{
+	if (GetWorld())
+	{
+		if (!GetWorld()->GetTimerManager().IsTimerActive(TimerHandle_Mashed))
+		{
+			Super::InteractionStopped(TimeStarted, PressedKey);
+		}
+	}
+}
+
+void UActorInteractableComponentMash::InteractionCanceled()
+{
+	Super::InteractionCanceled();
+
+	CleanUpComponent();
+}
+
+void UActorInteractableComponentMash::InteractionCompleted(const float& TimeCompleted)
+{
+	if (ActualMashAmount >= MinMashAmountRequired)
+	{
+		Super::InteractionCompleted(TimeCompleted);
+	}
+	else
+	{
+		OnInteractionFailed.Broadcast();
+	}
+
+	CleanUpComponent();
+}
+
+int32 UActorInteractableComponentMash::GetMinMashAmountRequired() const
+{ return MinMashAmountRequired; }
+
+void UActorInteractableComponentMash::SetMinMainAmountRequired(const int32 Value)
+{
+	MinMashAmountRequired = FMath::Max(Value, 2);
+}
+
+float UActorInteractableComponentMash::GetKeystrokeTimeThreshold() const
+{ return KeystrokeTimeThreshold; }
+
+void UActorInteractableComponentMash::SetKeystrokeTimeThreshold(const float Value)
+{
+	KeystrokeTimeThreshold = FMath::Max(Value, 0.01f);
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/ActorInteractionPlugin/Public/Components/ActorInteractableComponentMash.h
+++ Source/ActorInteractionPlugin/Public/Components/ActorInteractableComponentMash.h
@@ -6,6 +6,7 @@
 #include "ActorInteractableComponentBase.h"
 #include "ActorInteractableComponentMash.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInteractionFailed);
 
 UCLASS(ClassGroup=(Interaction), Blueprintable, hideCategories=(Collision, AssetUserData, Cooking, Activation), meta=(BlueprintSpawnableComponent, DisplayName = "Interactable Component Mash"))
 class ACTORINTERACTIONPLUGIN_API UActorInteractableComponentMash : public UActorInteractableComponentBase
@@ -19,5 +20,65 @@ public:
 protected:
 	
 	virtual void BeginPlay() override;
+	
+	virtual void InteractionStarted(const float& TimeStarted, const FKey& PressedKey) override;
+	virtual void InteractionStopped(const float& TimeStarted, const FKey& PressedKey) override;
+	virtual void InteractionCanceled() override;
+	virtual void InteractionCompleted(const float& TimeCompleted) override;
 
+protected:
+	
+	/**
+	 * Event called from OnInteractionFailed
+	 */
+	UFUNCTION(BlueprintImplementableEvent, Category="Interaction")
+	void OnInteractionFailedEvent();
+	UFUNCTION()
+	virtual void InteractionFailed();
+	virtual void OnInteractionFailedCallback();
+
+	void CleanUpComponent();
+	
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Interaction")
+	int32 GetMinMashAmountRequired() const;
+	UFUNCTION(BlueprintCallable, Category="Interaction")
+	void SetMinMainAmountRequired(const int32 Value);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Interaction")
+	float GetKeystrokeTimeThreshold() const;
+	UFUNCTION(BlueprintCallable, Category="Interaction")
+	void SetKeystrokeTimeThreshold(const float Value);
+	
+protected:
+	
+	/**
+	 * How many time the button must be pressed to accept the interaction as completed.
+	 */
+	UPROPERTY(SaveGame, EditAnywhere, BlueprintReadOnly,  Category="Interaction|Required", meta=(Units = "times", UIMin = 2, ClampMin = 2))
+	int32 MinMashAmountRequired;
+
+	/**
+	 * How much time it is allowed between each Key stroke before interaction fails.
+	 */
+	UPROPERTY(SaveGame, EditAnywhere, BlueprintReadOnly,  Category="Interaction|Required", meta=(Units = "s", UIMin = 0.01, ClampMin = 0.01))
+	float KeystrokeTimeThreshold;
+
+protected:
+
+	FTimerHandle TimerHandle_Mashed;
+
+	UPROPERTY(VisibleAnywhere, Category="Interaction|Read Only")
+	int32 ActualMashAmount;
+	
+protected:
+
+	/**
+	 * Event called once Interaction Mash fails.
+	 * Either because of Time is out or not enough Key strokes are registered.
+	 * Calls OnInteractionFailedEvent.
+	 */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnInteractionFailed OnInteractionFailed;
 };
