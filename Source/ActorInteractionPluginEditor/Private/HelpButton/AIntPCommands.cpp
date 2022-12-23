c8d201cbe038eb4469b07972588c453deb8c3a2f
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-12-23 16:57:54 +0100

    Update AIntPCommands.cpp

--- Source/ActorInteractionPluginEditor/Private/HelpButton/AIntPCommands.cpp
+++ Source/ActorInteractionPluginEditor/Private/HelpButton/AIntPCommands.cpp
@@ -7,7 +7,7 @@
 
 void FAIntPCommands::RegisterCommands()
 {
-	UI_COMMAND(PluginAction, "Support", "Opens Support channel", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control|EModifierKey::Shift|EModifierKey::Alt, EKeys::X));
+	UI_COMMAND(PluginAction, "Support", "Opens Mountea Framework Support channel", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control|EModifierKey::Shift|EModifierKey::Alt, EKeys::X));
 }
 
 #undef LOCTEXT_NAMESPACE
